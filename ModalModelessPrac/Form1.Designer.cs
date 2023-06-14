e508128a12aab0a37c8a45628b38ce999d5a22fb
Insori <s2110@e-mirim.hs.kr>
2023-06-14 11:43:18 +0900

    새 폼 생성 및 모달리스트 띄우기

--- ModalModelessPrac/Form1.Designer.cs
+++ ModalModelessPrac/Form1.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
@@ -41,14 +42,26 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(24, 167);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(184, 117);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(410, 458);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.button2_Click);
             this.ResumeLayout(false);
 
         }
@@ -56,6 +69,7 @@
         #endregion
 
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- ModalModelessPrac/Form1.cs
+++ ModalModelessPrac/Form1.cs
@@ -12,6 +12,14 @@ namespace ModalModelessPrac
 {
     public partial class Form1 : Form
     {
+        class CustomForm : Form 
+        {
+            public CustomForm()
+            {
+                this.Text = "제목 글자";
+
+            }
+        }
         public Form1()
         {
             InitializeComponent();
@@ -32,6 +40,12 @@ namespace ModalModelessPrac
                 MessageBox.Show("사용자가 취소하였습니다.", "알림",
                     MessageBoxButtons.OK, MessageBoxIcon.Hand);
             }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            CustomForm form = new CustomForm();
+            form.Show();
         }
     }
 }
